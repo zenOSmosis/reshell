9dfb2071f3815a61448355d5d013e7dd1321ae1a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-26 06:16:52 -0600

    Prototype example app lazy loading

---
+++ src/components/core/BaseView/BaseView.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function BaseView({ baseApp }) {
+  const [areBaseStylesLoaded, setAreBaseStylesLoaded] = useState(false);
+
+  useEffect(() => {
+    // Lazy-load BaseView style so it doesn't override before the app is mounted
+    //
+    // TODO: Use preload
+    import("./base-styles.css")
+      .then(() => setAreBaseStylesLoaded(true))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!areBaseStylesLoaded) {
+    return null;
+  }
+
+  const BaseAppView = baseApp;
+  return <BaseAppView />;
+}
