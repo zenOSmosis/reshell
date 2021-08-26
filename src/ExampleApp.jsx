45766ffd393f6281012ded0f2e558aec3c9b5a5d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-26 06:50:36 -0600

    Import layout components and prototype example app

--- src/ExampleApp.jsx
+++ src/ExampleApp.jsx
@@ -1,8 +1,27 @@
+import Layout, { Content, Footer } from "./components/Layout";
+import Center from "./components/Center";
+
 export default function ExampleApp() {
   return (
-    <div>
-      ReShell - An opinionated, paradigm-switching app mounter
-      <button>Switch Paradigms</button>
-    </div>
+    <Layout style={{ backgroundColor: "#424242", color: "#999" }}>
+      <Content>
+        <Center>
+          <div style={{ fontSize: "18.4vw", fontStyle: "italic" }}>ReShell</div>
+          <div style={{ fontSize: "4vw" }}>
+            An opinionated, paradigm-switching app mounter
+          </div>
+        </Center>
+      </Content>
+      <Footer>
+        <div style={{ padding: 4 }}>
+          If you are seeing this screen, ReShell is up and running. Get started
+          with{" "}
+          <a href="#" style={{ color: "orange" }}>
+            wrapping your app with ReShell
+          </a>
+          .
+        </div>
+      </Footer>
+    </Layout>
   );
 }
