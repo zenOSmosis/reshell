a6b2e6045d2a010df6b9d0e937a4b2647276f2d2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-07 08:43:38 -0600

    Add TODOs for Xterm

---
+++ src/portals/ExamplePortal/windows/Xterm/TODO.js
@@ -0,0 +1,15 @@
+/**
+ * Xterm.js
+ * https://www.npmjs.com/package/xterm
+ *
+ * TODO: Use attach addon for websocket support? https://github.com/xtermjs/xterm.js/blob/master/addons/xterm-addon-attach/src/AttachAddon.ts
+ *
+ * TODO: Use fit addon: https://github.com/xtermjs/xterm.js/tree/master/addons/xterm-addon-fit
+ * TODO: Define Terminal service?
+ *
+ * Previous implementation:
+ * - https://github.com/zenOSmosis/js-shell/blob/master/frontend/src/components/XTerm/XTerm.jsx
+ * - https://github.com/zenOSmosis/js-shell/blob/master/backend/src/api/socket.io/routes/socketChannel/createXTermSocketChannel.js
+ * - https://github.com/zenOSmosis/js-shell/blob/master/backend/src/utils/socketAPI/SocketChannel.js
+ * - https://github.com/zenOSmosis/js-shell/blob/master/backend/src/utils/socketAPI/handleSocketAPIRoute.js
+ */
