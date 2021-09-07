4ebd67c709c628ba3fcd7ad428bd0efb37d918e4
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-06 18:53:20 -0600

    Add demo slides

--- src/portals/ExamplePortal/windows/PresentationExpress/demo.slides.jsx
+++ src/portals/ExamplePortal/windows/PresentationExpress/demo.slides.jsx
@@ -3,17 +3,62 @@ import Center from "@components/Center";
 const slides = [
   {
     view: function View() {
-      return <Center>Example 1</Center>;
+      return <Center>ReShell has another project name</Center>;
     },
   },
   {
     view: function View() {
-      return <Center>Example 2</Center>;
+      return <Center>It's called "ESUI"</Center>;
     },
   },
   {
     view: function View() {
-      return <Center>Example 3</Center>;
+      return (
+        <Center>
+          It's called "ESUI"
+          <br />
+          It's pronounced "es-wee"
+        </Center>
+      );
+    },
+  },
+  {
+    view: function View() {
+      return <Center>ESUI: EcmaScript User Interface)</Center>;
+    },
+  },
+  {
+    view: function View() {
+      return (
+        <Center>
+          ESUI: EcmaScript User Interface)
+          <br />
+          (it also means "eat" in Latin)
+        </Center>
+      );
+    },
+  },
+  {
+    view: function View() {
+      return (
+        // TODO: Applying notebook paper, or code, view to this would look kind of cool
+        <Center>
+          {`
+            // Maybe we can say, "we built a rendering engine on top of React, so that
+            // React components can be used, without the complexity of dealing with React
+            // hook dependencies, in order to reduce excessive rendering, or stale states,
+            // caused by improper user of hook dependencies".... could also smooth the
+            // transition over to Svelte or any other rendering engine for apps created on
+            // top of one of them.
+            `
+            .split("\n")
+            .map((line, idx) => (
+              <div key={idx} style={{ textAlign: "left" }}>
+                {line}
+              </div>
+            ))}
+        </Center>
+      );
     },
   },
 ];
