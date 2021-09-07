a890168d06f3ae7fab91f87c2ec762d35c68719f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-06 19:14:19 -0600

    Add ReShell as final slide

--- src/portals/ExamplePortal/windows/PresentationExpress/demo.slides.jsx
+++ src/portals/ExamplePortal/windows/PresentationExpress/demo.slides.jsx
@@ -83,6 +83,17 @@ const slides = [
       );
     },
   },
+  {
+    view: function View() {
+      return (
+        <iframe
+          title="ReShell"
+          src="/"
+          style={{ width: "100%", height: "100%", border: 0 }}
+        />
+      );
+    },
+  },
 ];
 
 export default slides;
