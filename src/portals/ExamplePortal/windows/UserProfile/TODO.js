4827ab54cd35565eec72331a4630f58cc87f997d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-08 11:56:53 -0500

    Prototype user profile TODOs

---
+++ src/portals/ExamplePortal/windows/UserProfile/TODO.js
@@ -0,0 +1,2 @@
+// TODO: Work out how to save user profiles (local storage?  Make local storage service / Mongo?  encrypt: https://www.npmjs.com/package/mongoose-encryption)
+// TODO: Implement ability to save assets w/ user profile (leading into OpenFS / home directory?)
