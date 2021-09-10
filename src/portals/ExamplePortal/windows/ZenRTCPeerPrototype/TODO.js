8688cbbc03009cc129978055436e4b92597de8e5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-10 11:18:11 -0500

    Add TODO for ZenRTCPeerPrototype

---
+++ src/portals/ExamplePortal/windows/ZenRTCPeerPrototype/TODO.js
@@ -0,0 +1,6 @@
+// TODO: Build tooling to integrate webrtc-peer / Speaker.app's ZenRTCPeer
+// (test it, then split into separate repo)
+// TODO: Implement ServiceCore capabilities for reporting, and establish way
+// to view ZenRTCPeer connections within the reporter
+// TODO: Build UI /service tooling to listen as virtual server or as direct P2P
+// TODO: Show documentation in UI
