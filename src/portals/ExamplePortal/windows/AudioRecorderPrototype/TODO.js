f1bfd7e37c699ae0b381f0ff3acb48fbb14e8914
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-10 15:39:39 -0500

    Add TODOs for audio recording

---
+++ src/portals/ExamplePortal/windows/AudioRecorderPrototype/TODO.js
@@ -0,0 +1,4 @@
+// TODO: Use media-stream-track-controller to record audio
+// TODO: How much can we fit in memory; which formats can we save as natively, per platform?
+// TODO: Implementing recording of feeds captured in other applications (i.e. VU Meter, etc)
+// TODO: Implement separate audio mixer (i.e. track leveler) application
