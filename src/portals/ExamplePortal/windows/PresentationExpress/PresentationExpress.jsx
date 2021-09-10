80d7ad2c89fe5267dc161fa3039d94e15f73b610
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-10 17:27:01 -0500

    Resize default PresentationExpress window

--- src/portals/ExamplePortal/windows/PresentationExpress/PresentationExpress.jsx
+++ src/portals/ExamplePortal/windows/PresentationExpress/PresentationExpress.jsx
@@ -15,8 +15,8 @@ const PresentationExpress = {
   style: {
     left: "auto",
     bottom: 0,
-    width: 640,
-    height: 480,
+    width: 640 * 1.2,
+    height: 480 * 1.2,
   },
   view: function View() {
     const [slideIdx, setSlideIdx] = useState(0);
