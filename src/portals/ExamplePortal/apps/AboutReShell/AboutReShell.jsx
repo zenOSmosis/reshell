d7e5268bcd54ce5e7c3cda21f4e8d5fc2f0e62b9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-18 09:24:29 -0600

    Add TODOs

--- src/portals/ExamplePortal/apps/AboutReShell/AboutReShell.jsx
+++ src/portals/ExamplePortal/apps/AboutReShell/AboutReShell.jsx
@@ -1,6 +1,21 @@
 import Layout, { Content, Footer } from "@components/Layout";
 import Center from "@components/Center";
 
+// TODO: Include ability to parse out an HTML file and dynamically link it to
+// the public directory during runtime (and during build) for SEO
+
+// TODO: Enable any application to include about info, metadata, so that they
+// can be included in SEO
+
+// TODO: Don't automatically initialize ReShell when a search engine is
+// detected (i.e. look for various JS properties a search engine might not
+// exhibit and test if those capabilities exist in order to determine if a
+// search engine or not)
+
+// TODO: Try to not get too fancy with SEO pages, and keep them mostly HTML,
+// with JS assets sprinkled in to be able to open relevant parts of ReShell
+// when needed, if it is not automatically initialized
+
 const AboutReShell = {
   id: "about-reshell",
   title: "About ReShell",
