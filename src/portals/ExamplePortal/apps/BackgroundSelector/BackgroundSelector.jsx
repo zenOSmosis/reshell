46d6701144be587ddcd155678b4d58e5fe938acc
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-18 10:06:51 -0600

    Add TODOs to BackgroundSelector

--- src/portals/ExamplePortal/apps/BackgroundSelector/BackgroundSelector.jsx
+++ src/portals/ExamplePortal/apps/BackgroundSelector/BackgroundSelector.jsx
@@ -1,2 +1,6 @@
 // TODO: Include ability to use video streams captured by media-stream-track-controller as the background (show as thumbnails)
 // TODO: Use default Desktop backgroundView as a thumbnail
+
+// TODO: Use particles as potential background
+// TODO: Use solid colors as potential background
+// TODO: Allow user-defined URL as potential background
