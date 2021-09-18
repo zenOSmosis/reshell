3fde3b1104f37a02107dbd9776fb32ee04eba81d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-18 09:24:49 -0600

    Prototype BackgroundSelector ideas

---
+++ src/portals/ExamplePortal/apps/BackgroundSelector/BackgroundSelector.jsx
@@ -0,0 +1,2 @@
+// TODO: Include ability to use video streams captured by media-stream-track-controller as the background (show as thumbnails)
+// TODO: Use default Desktop backgroundView as a thumbnail
