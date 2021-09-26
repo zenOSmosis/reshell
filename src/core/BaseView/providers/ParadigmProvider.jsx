9fc1b85b771e1980bfc7ced6b2626d0ec4b7b210
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-25 19:59:12 -0600

    Add TODO to ParadigmProvider

--- src/core/BaseView/providers/ParadigmProvider.jsx
+++ src/core/BaseView/providers/ParadigmProvider.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export const ParadigmContext = React.createContext({});
 
 // TODO: Add desktop paradigm
-// TODO: Add mobile paradigm
-// TODO: Add card paradigm: https://codesandbox.io/embed/j0y0vpz59
+// TODO: Add mobile paradigm (half-quadrant snapping; scroll dock / scroll side-by-side-apps / views able to be split vertically / horizontally, and windows able to be dragged into quadrants / scaling optional)
 
 export default function ParadigmProvider({ children }) {
   // TODO: Work out paradigm values; use PhantomCollection for paradigm management
