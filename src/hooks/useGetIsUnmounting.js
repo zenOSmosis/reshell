747d6b258a587f5e514440d5c3f60f547a54c5cc
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-02 12:07:27 -0600

    Fix issue where closing screen capture window could throw error related to potential memory leak

---
+++ src/hooks/useGetIsUnmounting.js
@@ -0,0 +1,16 @@
+import { useCallback, useEffect, useRef } from "react";
+
+// TODO: Document
+export default function useGetIsUnmounting() {
+  const refIsUnmounting = useRef(false);
+
+  useEffect(() => {
+    return function unmount() {
+      refIsUnmounting.current = true;
+    };
+  });
+
+  const getIsUnmounting = useCallback(() => refIsUnmounting.current, []);
+
+  return getIsUnmounting;
+}
