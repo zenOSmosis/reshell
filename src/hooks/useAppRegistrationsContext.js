a5ae9ed0e93799552147a3c9b41040c4ef514861
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-02 15:04:57 -0600

    Add TODOs to move service, registration, and runtime hooks into core

--- src/hooks/useAppRegistrationsContext.js
+++ src/hooks/useAppRegistrationsContext.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { AppRegistrationsContext } from "@core/BaseView/providers/AppRegistrationsProvider";
 
+// TODO: Move to @core/hooks
+
 export default function useAppRegistrationsContext() {
   return useContext(AppRegistrationsContext);
 }
