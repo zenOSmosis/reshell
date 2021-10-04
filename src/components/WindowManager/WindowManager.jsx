f1267ce97c447f419b1ed23ea0bd4668dd30aabc
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-04 06:58:10 -0600

    Improve service update refresh handling and remove non-utilized props in WindowManager WrappedView

--- src/components/WindowManager/WindowManager.jsx
+++ src/components/WindowManager/WindowManager.jsx
@@ -10,6 +10,7 @@ import useServicesContext from "@hooks/useServicesContext";
 import useDesktopContext from "@hooks/useDesktopContext";
 import useAppRegistrationsContext from "@hooks/useAppRegistrationsContext";
 import useAppRuntimesContext from "@hooks/useAppRuntimesContext";
+import useForceUpdate from "@hooks/useForceUpdate";
 
 import useRegistrationViewOnResized from "./hooks/useRegistrationViewOnResized";
 
@@ -384,21 +385,18 @@ function WindowManagerView({ appDescriptors = [], children }) {
 // TODO: Document; rename? (NOTE: this wrapped view was designed to make it
 // easier to make the wrapping view render out-of-sequence with the containing
 // view, such as when a service updates, etc.)
-function WrappedView({
+const WrappedView = React.memo(function WrappedView({
   appServices,
   windowController,
   appRuntime,
   view: ViewComponent,
-  windowSwitchToAppRegistrationID,
-  ...rest
 }) {
-  // TODO: Document
-  const [serviceUpdateIdx, setServiceUpdateIdx] = useState(0);
+  const forceUpdate = useForceUpdate();
 
   // Re-render window when a service updates
   useEffect(() => {
     const _handleServiceUpdate = () => {
-      setServiceUpdateIdx(prev => prev + 1);
+      forceUpdate();
     };
 
     for (const service of Object.values(appServices)) {
@@ -412,20 +410,17 @@ function WrappedView({
         service.off(EVT_UPDATED, _handleServiceUpdate);
       }
     };
-  }, [appServices]);
+  }, [appServices, forceUpdate]);
 
   // TODO: Document
   const setResizeHandler = useRegistrationViewOnResized(windowController);
 
   return (
     <ViewComponent
-      {...rest}
       windowController={windowController}
       appServices={appServices}
       appRuntime={appRuntime}
       setResizeHandler={setResizeHandler}
-      // Force update every time service updates
-      serviceUpdateIdx={serviceUpdateIdx}
     />
   );
-}
+});
