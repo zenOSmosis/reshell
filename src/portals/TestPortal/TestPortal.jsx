d6a6ca25b32b9cc3714bf8ead8a3c6a67758b712
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-09 10:06:16 -0600

    Add ServiceMonitor to TestPortal

--- src/portals/TestPortal/TestPortal.jsx
+++ src/portals/TestPortal/TestPortal.jsx
@@ -2,6 +2,7 @@ import Desktop from "@components/Desktop";
 
 import Applications from "../ExamplePortal/apps/Applications";
 import ApplicationMonitor from "../ExamplePortal/apps/ApplicationMonitor";
+import ServiceMonitor from "../ExamplePortal/apps/ServiceMonitor";
 import AdditionWindow from "./apps/AdditionWindow";
 import MultiplicationWindow from "./apps/MultiplicationWindow";
 import TotalsWindow from "./apps/TotalsWindow";
@@ -13,6 +14,7 @@ export default function WizardPortal() {
       appDescriptors={[
         Applications,
         ApplicationMonitor,
+        ServiceMonitor,
         AdditionWindow,
         MultiplicationWindow,
         TotalsWindow,
