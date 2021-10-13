ed786d3b9aaad9bd2b25c8628e74bbe31821efca
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-13 07:50:26 -0600

    Add TODOs and remove non-utilized property

--- src/components/Layout/RowColumn/RowColumn.jsx
+++ src/components/Layout/RowColumn/RowColumn.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classNames from "classnames";
 import styles from "./RowColumn.module.css";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 // @see https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01
 
+// TODO: Implement prop-types
 function Row({ children, className, ...rest }) {
   // TODO: Enforce that only Column components are direct children of this
 
@@ -15,14 +16,11 @@ function Row({ children, className, ...rest }) {
   );
 }
 
-Column.propTypes = {
-  isForcedMinWidth: PropTypes.bool,
-};
-
+// TODO: Implement prop-types
 /**
  * Evenly-sized column.
  */
-function Column({ children, className, isForcedMinWidth, ...rest }) {
+function Column({ children, className, ...rest }) {
   // TODO: Enforce that only Row component is direct parent of this
 
   return (
