f95fff89e22141d283d4760dc61742a12e2328b9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-13 16:13:23 -0600

    Add TODOs

---
+++ src/portals/ExamplePortal/apps/SystemInfo/TODO.js
@@ -0,0 +1,5 @@
+// TODO: Include list of package information, their respective versions, and links to their npm / github
+
+// TODO: Include list of memory info / cache storage / etc.
+
+// TODO: Include list of device metrics / etc.
