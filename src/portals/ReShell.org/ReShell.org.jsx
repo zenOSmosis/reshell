fc47f60b3ea80cbe30c5daa7c1bd00c18184adb2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-14 18:24:03 -0600

    Add commented-out ApplicationMonitor

--- src/portals/ReShell.org/ReShell.org.jsx
+++ src/portals/ReShell.org/ReShell.org.jsx
@@ -1,6 +1,7 @@
 import Desktop from "@components/Desktop";
 
 import Applications from "../ExamplePortal/apps/Applications";
+// import ApplicationMonitor from "../ExamplePortal/apps/ApplicationMonitor";
 import AboutReShell from "../ExamplePortal/apps/AboutReShell";
 import ServiceMonitor from "../ExamplePortal/apps/ServiceMonitor";
 
@@ -10,6 +11,13 @@ import ServiceMonitor from "../ExamplePortal/apps/ServiceMonitor";
 
 export default function ReShellOrgPortal() {
   return (
-    <Desktop appDescriptors={[Applications, AboutReShell, ServiceMonitor]} />
+    <Desktop
+      appDescriptors={[
+        Applications,
+        // ApplicationMonitor,
+        AboutReShell,
+        ServiceMonitor,
+      ]}
+    />
   );
 }
