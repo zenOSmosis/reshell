8c3d1990102ffe3a370ef3031c3d70e3682323ad
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-16 11:13:49 -0600

    Fix hasSiblings calculation

--- src/components/Padding/Padding.jsx
+++ src/components/Padding/Padding.jsx
@@ -10,9 +10,7 @@ export default function Padding({ children, className, ...rest }) {
   // Auto-determine if the element has siblings
   useEffect(() => {
     if (el) {
-      const hasSiblings = Boolean(
-        el.nextElementSibling || el.lastElementSibling
-      );
+      const hasSiblings = Boolean([...el.parentNode.children].length > 1);
 
       setHasSiblings(hasSiblings);
     }
