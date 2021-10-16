914d21c140b6053c706c59f88c5166491ef142c5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-16 10:40:43 -0600

    Prototype tech stack rendering

--- src/portals/ExamplePortal/apps/AboutReShell/views/Resources/Resources.jsx
+++ src/portals/ExamplePortal/apps/AboutReShell/views/Resources/Resources.jsx
@@ -1,5 +1,6 @@
 import Center from "@components/Center";
 import Padding from "@components/Padding";
+import { Row, Column } from "@components/Layout";
 
 const LINKS = {
   zenOSmosis: "https://zenOSmosis.com",
@@ -13,12 +14,38 @@ export default function Resources() {
       {
         // TODO: Display grid here for: PhantomCore, etc.
       }
+      <p style={{ fontWeight: "bold" }}>Some included technologies:</p>
+      <Row style={{ height: 50 }}>
+        <Column>
+          <button>PhantomCore</button>
+        </Column>
+        <Column>
+          <button>PhantomCollection</button>
+        </Column>
+        <Column>
+          <button>ReShell Core</button>
+        </Column>
+      </Row>
+      <Row style={{ height: 50 }}>
+        <Column>
+          <button>SyncObject</button>
+        </Column>
+        <Column>
+          <button>MediaStream Controller</button>
+        </Column>
+        <Column>
+          <button>webrtc-peer</button>
+        </Column>
+      </Row>
 
       <p style={{ fontWeight: "bold" }}>Additional resources:</p>
       <div style={{ display: "inline-block", textAlign: "left" }}>
         {Object.entries(LINKS).map(([name, url], idx) => (
           <Padding key={idx}>
             {name}:{" "}
+            {
+              // TODO: Use ExternalLink component for this
+            }
             <a href={url} target="_blank" rel="noreferrer">
               {url}
             </a>
