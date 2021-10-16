cde209b1d36210a50e3f54a7b788a7717f4194ec
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-16 09:44:37 -0600

    Prototype AboutReShell resources tab

--- src/portals/ExamplePortal/apps/AboutReShell/AboutReShell.jsx
+++ src/portals/ExamplePortal/apps/AboutReShell/AboutReShell.jsx
@@ -2,6 +2,9 @@ import Padding from "@components/Padding";
 import Layout, { Content, Footer } from "@components/Layout";
 import Center from "@components/Center";
 import AutoScaler from "@components/AutoScaler";
+import ButtonPanel from "@components/ButtonPanel";
+
+import Resources from "./views/Resources";
 
 import getCopyright from "@utils/getCopyright";
 
@@ -24,30 +27,81 @@ const AboutReShell = {
     width: 640,
     height: 480,
   },
-  view: function View() {
+  initialSharedState: {
+    screen: "overview",
+  },
+  titleBarView: function TitleBarView({ sharedState, setSharedState }) {
+    /*
+    const handleSetSearchQuery = useCallback(
+      evt => setSharedState({ searchQuery: evt.target.value }),
+      [setSharedState]
+    );
+
+    const handleDoubleClick = useCallback(
+      evt => {
+        // Prevent double-clicks from resizing the window if there is text in
+        // the search bar
+        if (Boolean(sharedState.searchQuery)) {
+          evt.stopPropagation();
+        }
+      },
+      [sharedState]
+    );
+    */
+
+    return (
+      <Padding className="button-group">
+        <ButtonPanel
+          buttons={[
+            {
+              content: "Overview",
+              onClick: () => setSharedState({ screen: "overview" }),
+            },
+            {
+              content: "Resources",
+              onClick: () => setSharedState({ screen: "resources" }),
+            },
+          ]}
+        />
+      </Padding>
+    );
+  },
+  view: function View({ sharedState }) {
     return (
       <Padding>
         <Layout>
           <Content>
-            <AutoScaler>
-              <Center>
-                <div style={{ fontSize: "8rem", fontStyle: "italic" }}>
-                  ReShell
-                </div>
-                <div style={{ fontSize: "1.5rem" }}>
-                  App layout framework and UI services engine
-                </div>
-              </Center>
-            </AutoScaler>
+            {sharedState.screen === "overview" && (
+              <AutoScaler>
+                <Center>
+                  <div style={{ fontSize: "8rem", fontStyle: "italic" }}>
+                    ReShell
+                  </div>
+                  <div style={{ fontSize: "1.5rem" }}>
+                    App layout framework and UI services engine
+                  </div>
+                </Center>
+              </AutoScaler>
+            )}
+
+            {sharedState.screen === "resources" && <Resources />}
           </Content>
           <Footer style={{ textAlign: "center" }}>
-            <p>
-              This project is a work-in-progress. For contact information, visit{" "}
-              <a href="https://zenOSmosis.com" target="_blank" rel="noreferrer">
-                zenOSmosis.com
-              </a>
-              .
-            </p>
+            {sharedState.screen === "overview" && (
+              <p>
+                This project is a work-in-progress. For contact information,
+                visit{" "}
+                <a
+                  href="https://zenOSmosis.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  zenOSmosis.com
+                </a>
+                .
+              </p>
+            )}
+
             <div style={{ opacity: 0.5 }}>{getCopyright()}</div>
 
             {/*
---
+++ src/portals/ExamplePortal/apps/AboutReShell/views/Resources/Resources.jsx
@@ -0,0 +1,26 @@
+import Center from "@components/Center";
+import Padding from "@components/Padding";
+
+const LINKS = {
+  zenOSmosis: "https://zenOSmosis.com",
+  GitHub: "https://github.com/zenosmosis",
+  "Speaker.app": "https://speaker.app",
+};
+
+export default function Resources() {
+  return (
+    <Center canOverflow={true}>
+      <p style={{ fontWeight: "bold" }}>Additional resources:</p>
+      <div style={{ display: "inline-block", textAlign: "left" }}>
+        {Object.entries(LINKS).map(([name, url], idx) => (
+          <Padding key={idx}>
+            {name}:{" "}
+            <a href={url} target="_blank" rel="noreferrer">
+              {url}
+            </a>
+          </Padding>
+        ))}
+      </div>
+    </Center>
+  );
+}
