6c8d81abccab61444776854070f11f217d340c07
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-16 14:23:00 -0600

    Rework latest version detection

--- src/utils/fetchIsLatestVersion.js
+++ src/utils/fetchIsLatestVersion.js
@@ -1,41 +1,22 @@
 import axios from "axios";
 
 // TODO: Document
-//
-// IMPORTANT: This is set up to only work with create-react-app and may need
-// to be modified otherwise
 export default async function fetchIsLatestVersion() {
-  // CRA seems to alter the directory structure, per request, so we have to
-  // skip it in development
-  if (process.env.NODE_ENV === "development") {
-    return true;
-  }
-
   // TODO: Parse out local HTML, analyzing static JS links
-  const ours = _getStaticTags();
+  const ours = await fetchAssetManifest(false);
 
-  const result = await axios.get(`/?__t=${new Date().getTime()}`);
-  const resultDOM = new window.DOMParser(result.data);
+  const theirs = await fetchAssetManifest(true);
 
-  const theirs = _getStaticTags(resultDOM.document);
-
-  for (const src of theirs) {
-    console.log({ src, ours });
-
-    if (!ours.includes(src)) {
-      return false;
-    }
-  }
-
-  return true;
+  return JSON.stringify(ours) === JSON.stringify(theirs);
 }
 
 // TODO: Document
-function _getStaticTags(document = window.document) {
-  const jsTags = [...document.getElementsByTagName("script")];
-  const staticJsTagSrcs = jsTags
-    .filter(tag => tag.getAttribute("src")?.startsWith("/static"))
-    .map(tag => tag.getAttribute("src"));
+async function fetchAssetManifest(isTheirs = true) {
+  // TODO: Include optional homepage parameter if we're not basing off of root
+  const response = await axios.get(
+    // NOTE: Non-CRA (create-react-app) usage may need this to be modified accordingly
+    `/asset-manifest.json${isTheirs ? `?__t=${new Date().getTime()}` : ""}`
+  );
 
-  return staticJsTagSrcs;
+  return response.data;
 }
