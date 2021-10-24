ed1d05dde18acc969ad794bd102b7b59d2ba5022
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-23 18:53:32 -0600

    Fix issue where window titlebar buttons would appear larger when focused

--- src/components/Window/Window.Titlebar/Window.Titlebar.Button/Window.Titlebar.Button.jsx
+++ src/components/Window/Window.Titlebar/Window.Titlebar.Button/Window.Titlebar.Button.jsx
@@ -1,7 +1,5 @@
 import styles from "../../Window.module.css";
 
-// TODO: Fix issue where focused buttons have border, making them appear larger than they should
-
 export function WindowTitlebarMaximizeButton({ ...rest }) {
   return <WindowTitlebarButton {...rest} className={styles["maximize"]} />;
 }
