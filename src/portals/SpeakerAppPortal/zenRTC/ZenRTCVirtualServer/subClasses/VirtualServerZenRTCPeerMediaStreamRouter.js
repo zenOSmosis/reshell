e75e736f79d49131241ad79039a5e7c1aa67077f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-11-25 19:42:26 -0600

    Fix incorrect fullSync call and add comments

--- src/portals/SpeakerAppPortal/zenRTC/ZenRTCVirtualServer/subClasses/VirtualServerZenRTCPeerMediaStreamRouter.js
+++ src/portals/SpeakerAppPortal/zenRTC/ZenRTCVirtualServer/subClasses/VirtualServerZenRTCPeerMediaStreamRouter.js
@@ -1,7 +1,17 @@
 import { PhantomCollection } from "phantom-core";
 import VirtualServerZenRTCPeer from "./VirtualServerZenRTCPeer";
 
+/**
+ * Performs media stream routing for all of the peers.
+ */
 export default class VirtualServerZenRTCPeerMediaStreamRouter extends PhantomCollection {
+  /**
+   * Adds a VirtualServerZenRTCPeer instance to the router collection and sets
+   * up an initial full media stream sync.
+   *
+   * @param {VirtualServerZenRTCPeer} virtualServerZenRTCPeer
+   * @return {void}
+   */
   addChild(virtualServerZenRTCPeer) {
     if (!(virtualServerZenRTCPeer instanceof VirtualServerZenRTCPeer)) {
       throw new TypeError(
@@ -9,43 +19,61 @@ export default class VirtualServerZenRTCPeerMediaStreamRouter extends PhantomCol
       );
     }
 
+    // Duplicate attempts will be silently ignored
     super.addChild(virtualServerZenRTCPeer);
 
-    // Route other peer's media streams to this one
-    (() => {
-      const otherPeers = this.getChildren().filter(
-        pred => pred !== virtualServerZenRTCPeer
-      );
-
-      const outgoingMediaStreams =
-        virtualServerZenRTCPeer.getOutgoingMediaStreams();
-
-      for (const otherPeer of otherPeers) {
-        // Add the other peers' media streams to this peer
-        for (const mediaStream of otherPeer.getIncomingMediaStreams()) {
-          for (const mediaStreamTrack of mediaStream.getTracks()) {
-            virtualServerZenRTCPeer.addOutgoingMediaStreamTrack(
-              mediaStreamTrack,
-              mediaStream
-            );
-          }
-        }
-
-        // Route this peer's media streams to the other peers
-        for (const mediaStream of outgoingMediaStreams) {
-          for (const mediaStreamTrack of mediaStream.getTracks()) {
-            this.addOutgoingMediaStreamTrack(
-              otherPeer,
-              mediaStreamTrack,
-              mediaStream
-            );
-          }
-        }
-      }
-    })();
+    // IMPORTANT: This must come after super.addChild as other peers are
+    // determined by the collection's children
+    this.fullSync(virtualServerZenRTCPeer);
   }
 
-  // TODO: Document
+  /**
+   * Performs a full media stream track sync from the given peer to / from the
+   * other peers.
+   *
+   * @param {VirtualServerZenRTCPeer} virtualServerZenRTCPeer
+   * @return {void}
+   */
+  fullSync(virtualServerZenRTCPeer) {
+    const otherPeers = this.getChildren().filter(
+      pred => pred !== virtualServerZenRTCPeer
+    );
+
+    const outgoingMediaStreams =
+      virtualServerZenRTCPeer.getOutgoingMediaStreams();
+
+    for (const otherPeer of otherPeers) {
+      // Add the other peers' media streams to this peer
+      for (const mediaStream of otherPeer.getIncomingMediaStreams()) {
+        for (const mediaStreamTrack of mediaStream.getTracks()) {
+          virtualServerZenRTCPeer.addOutgoingMediaStreamTrack(
+            mediaStreamTrack,
+            mediaStream
+          );
+        }
+      }
+
+      // Route this peer's media streams to the other peers
+      for (const mediaStream of outgoingMediaStreams) {
+        for (const mediaStreamTrack of mediaStream.getTracks()) {
+          this.addOutgoingMediaStreamTrack(
+            otherPeer,
+            mediaStreamTrack,
+            mediaStream
+          );
+        }
+      }
+    }
+  }
+
+  /**
+   * Sends a media stream track to the other peers except for the given
+   * VirtualServerZenRTCPeer.
+   *
+   * @param {VirtualServerZenRTCPeer} virtualServerZenRTCPeer
+   * @param {MediaStreamTrack} mediaStreamTrack
+   * @param {MediaStream} mediaStream
+   */
   addOutgoingMediaStreamTrack(
     virtualServerZenRTCPeer,
     mediaStreamTrack,
@@ -60,7 +88,14 @@ export default class VirtualServerZenRTCPeerMediaStreamRouter extends PhantomCol
     }
   }
 
-  // TODO: Document
+  /**
+   * Removes a media stream track from the other peers except for the given
+   * VirtualServerZenRTCPeer.
+   *
+   * @param {VirtualServerZenRTCPeer} virtualServerZenRTCPeer
+   * @param {MediaStreamTrack} mediaStreamTrack
+   * @param {MediaStream} mediaStream
+   */
   removeOutgoingMediaStreamTrack(
     virtualServerZenRTCPeer,
     mediaStreamTrack,
