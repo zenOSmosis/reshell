44f97b669b42e14d8f439b47a450b509110d63b2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-11-27 11:44:44 -0600

    Improve state routing

--- src/portals/SpeakerAppPortal/zenRTC/ZenRTCVirtualServer/subClasses/routers/VirtualServerZenRTCPeerPhantomPeerRouter.js
+++ src/portals/SpeakerAppPortal/zenRTC/ZenRTCVirtualServer/subClasses/routers/VirtualServerZenRTCPeerPhantomPeerRouter.js
@@ -26,15 +26,23 @@ export default class VirtualServerZenRTCPeerPhantomPeerRouter extends PhantomCol
 
     // IMPORTANT: This must come after super.addChild as other peers are
     // determined by the collection's children
-    this.fullSync(virtualServerZenRTCPeer);
+    this.sync(virtualServerZenRTCPeer);
   }
 
   // TODO: Document
   /**
+   * IMPORTANT: It is important to know that the underlying mechanism for this
+   * is SyncObject, which performs differential state updates across the wire.
+   *
+   * As such, it currently does not handle arrays and state deletions must be
+   * nulled out.
+   *
+   * @see https://github.com/zenOSmosis/sync-object
+   *
    * @param {VirtualServerZenRTCPeer} virtualServerZenRTCPeer
    * @return {void}
    */
-  fullSync(virtualServerZenRTCPeer) {
+  sync(virtualServerZenRTCPeer) {
     const otherPeers = this.getChildren().filter(
       pred => pred !== virtualServerZenRTCPeer
     );
@@ -47,41 +55,53 @@ export default class VirtualServerZenRTCPeerPhantomPeerRouter extends PhantomCol
     const ourClientSignalBrokerId =
       virtualServerZenRTCPeer.getClientSignalBrokerId();
 
-    // TODO: Remove
-    /*
-    console.log({
-      writableSyncObject: ourWritableSyncObject.getState(),
-      readOnlySyncObject: ourReadOnlySyncObject.getState(),
-    });
-    */
+    const ourReadOnlyState = virtualServerZenRTCPeer.getIsConnected()
+      ? ourReadOnlySyncObject.getState()
+      : // IMPORTANT: At this time, SyncObject does not support setting
+        // undefined and syncing over the wire, so null is used instead
+        //
+        // @see https://github.com/zenOSmosis/sync-object/issues/40
+        null;
 
     for (const otherPeer of otherPeers) {
       const theirWritableSyncObject = otherPeer.getWritableSyncObject();
       const theirReadOnlySyncObject = otherPeer.getReadOnlySyncObject();
 
+      // TODO: Prevent loop-back where our state is written to theirs, then written to ours
+
+      const theirReadOnlyState = otherPeer.getIsConnected()
+        ? theirReadOnlySyncObject.getState()
+        : // IMPORTANT: At this time, SyncObject does not support setting
+          // undefined and syncing over the wire, so null is used instead
+          //
+          // @see https://github.com/zenOSmosis/sync-object/issues/40
+          null;
+
+      // if (otherPeer.getIsConnected()) {
       // Write our state to the other clients
       theirWritableSyncObject.setState({
-        [ourClientSignalBrokerId]: virtualServerZenRTCPeer.getIsConnected()
-          ? ourReadOnlySyncObject.getState()
-          : undefined,
+        [ourClientSignalBrokerId]: ourReadOnlyState,
       });
+      // }
 
       const theirClientSignalBrokerId = otherPeer.getClientSignalBrokerId();
 
+      // if (virtualServerZenRTCPeer.getIsConnected()) {
+      // Write other client state to ours
       ourWritableSyncObject.setState({
-        [theirClientSignalBrokerId]: otherPeer.getIsConnected()
-          ? theirReadOnlySyncObject.getState()
-          : undefined,
+        [theirClientSignalBrokerId]: theirReadOnlyState,
       });
+      // }
     }
   }
 
-  // TODO: Implement and document
+  // TODO: Document
   handlePeerSharedStateUpdated(virtualServerZenRTCPeer) {
-    this.fullSync(virtualServerZenRTCPeer);
+    this.sync(virtualServerZenRTCPeer);
   }
 
+  // TODO: Document
   handlePeerDisconnect(virtualServerZenRTCPeer) {
-    this.fullSync(virtualServerZenRTCPeer);
+    this.sync(virtualServerZenRTCPeer);
   }
 }
