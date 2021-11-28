fe605e73526ba71cf3189b50af67baef5894c794
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-11-28 07:56:42 -0600

    Add TODO

--- src/components/NoWrap/NoWrap.jsx
+++ src/components/NoWrap/NoWrap.jsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import styles from "./NoWrap.module.css";
 
+// TODO: Document
 export default function NoWrap({ children, className, ...rest }) {
   return (
     <span {...rest} className={classNames(styles["no-wrap"], className)}>
