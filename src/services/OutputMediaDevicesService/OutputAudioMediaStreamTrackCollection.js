078834c7124c4ca4bc84c63b7638449a6a746f7c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-13 12:48:16 -0600

    Partially refactor OutputMediaDevicesService

---
+++ src/services/OutputMediaDevicesService/OutputAudioMediaStreamTrackCollection.js
@@ -0,0 +1,68 @@
+import PhantomCore, { PhantomCollection } from "phantom-core";
+
+// TODO: Change to ES6 imports once PhantomCore supports package.json exports
+// @see https://github.com/zenOSmosis/phantom-core/issues/98
+const { EVT_CHILD_INSTANCE_ADDED, EVT_CHILD_INSTANCE_REMOVED } =
+  PhantomCollection;
+
+export { EVT_CHILD_INSTANCE_ADDED, EVT_CHILD_INSTANCE_REMOVED };
+
+// TODO: Build out; ensuring added children are media device controller factories
+// class OutputMediaDeviceFactoryCollection extends PhantomCollection {
+// TODO: Ensure that added children are of MediaStreamTrackControllerFactory type
+// }
+export default class OutputAudioMediaStreamTrackCollection extends PhantomCollection {
+  // TODO: Document
+  addChild(mediaStreamTrack, mediaStream) {
+    // TODO: Remove
+    console.log("TODO: Handle addOutputMediaStreamTrack", {
+      mediaStreamTrack,
+      mediaStream,
+    });
+
+    if (mediaStreamTrack.kind === "audio") {
+      if (!this.getChildWithKey(mediaStreamTrack.id)) {
+        const phantomAudioWrapper = new PhantomCore();
+        phantomAudioWrapper.__audioMediaStreamTrack = mediaStreamTrack;
+
+        super.addChild(phantomAudioWrapper);
+      }
+    }
+  }
+
+  // TODO: Document
+  addOutputMediaStreamTrack(mediaStreamTrack, mediaStream) {
+    return this.addChild(mediaStreamTrack, mediaStream);
+  }
+
+  // TODO: Document
+  removeChild(mediaStreamTrack, mediaStream) {
+    // TODO: Remove
+    console.log("TODO: Handle removeOutputMediaStreamTrack", {
+      mediaStreamTrack,
+      mediaStream,
+    });
+
+    if (mediaStreamTrack.kind === "audio") {
+      const phantomAudioWrapper = this.getChildWithKey(mediaStreamTrack.id);
+
+      if (phantomAudioWrapper) {
+        phantomAudioWrapper.destroy();
+      }
+    }
+  }
+
+  // TODO: Document
+  removeOutputMediaStreamTrack(mediaStreamTrack, mediaStream) {
+    return this.removeChild(mediaStreamTrack, mediaStream);
+  }
+
+  /**
+   * @return {Promise<void>}
+   */
+  async destroy() {
+    await this.destroyAllChildren();
+
+    super.destroy();
+  }
+}
---
+++ src/services/OutputMediaDevicesService/OutputMediaDevicesService.js
@@ -0,0 +1,119 @@
+import UIServiceCore from "@core/classes/UIServiceCore";
+import OutputAudioMediaStreamTrackCollection, {
+  EVT_CHILD_INSTANCE_ADDED,
+  EVT_CHILD_INSTANCE_REMOVED,
+} from "./OutputAudioMediaStreamTrackCollection";
+
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { Audio } from "@components/audioVideoRenderers";
+
+// TODO: Document
+export default class OutputMediaDevicesService extends UIServiceCore {
+  constructor(...args) {
+    super(...args);
+
+    this.setTitle("Output Media Devices Service");
+
+    this.bindCollectionClass(OutputAudioMediaStreamTrackCollection);
+
+    // TODO: Refactor and document
+    (() => {
+      const audioMediaStreamTrackCollection = this.getCollectionInstance(
+        OutputAudioMediaStreamTrackCollection
+      );
+
+      function OutputMediaDevicesAudioAudio() {
+        const [audioMediaStreamTracks, setAudioMediaStreamTracks] = useState(
+          []
+        );
+
+        // Handle track syncing
+        useEffect(() => {
+          audioMediaStreamTrackCollection.on(
+            EVT_CHILD_INSTANCE_ADDED,
+            phantomAudioWrapper => {
+              // TODO: Remove
+              console.log("added phantom audio wrapper", phantomAudioWrapper);
+
+              setAudioMediaStreamTracks(
+                audioMediaStreamTrackCollection
+                  .getChildren()
+                  .map(
+                    phantomAudioWrapper =>
+                      phantomAudioWrapper.__audioMediaStreamTrack
+                  )
+              );
+            }
+          );
+
+          audioMediaStreamTrackCollection.on(
+            EVT_CHILD_INSTANCE_REMOVED,
+            phantomAudioWrapper => {
+              // TODO: Remove
+              console.log("removed phantom audio wrapper", phantomAudioWrapper);
+
+              setAudioMediaStreamTracks(
+                audioMediaStreamTrackCollection
+                  .getChildren()
+                  .map(
+                    phantomAudioWrapper =>
+                      phantomAudioWrapper.__audioMediaStreamTrack
+                  )
+              );
+            }
+          );
+        }, []);
+
+        // TODO: Remove
+        console.log({
+          audioMediaStreamTracks,
+        });
+
+        return (
+          <React.Fragment>
+            {audioMediaStreamTracks.map(track => {
+              return <Audio key={track.id} mediaStreamTrack={track} />;
+            })}
+          </React.Fragment>
+        );
+      }
+
+      const audioDOMBase = document.createElement("div");
+      audioDOMBase.style.display = "hidden";
+
+      window.document.body.appendChild(audioDOMBase);
+      this.registerShutdownHandler(() =>
+        window.document.body.removeChild(audioDOMBase)
+      );
+
+      ReactDOM.render(<OutputMediaDevicesAudioAudio />, audioDOMBase);
+    })();
+  }
+
+  // TODO: Document
+  addOutputMediaStreamTrack(mediaStreamTrack, mediaStream) {
+    // TODO: Use class property once https://github.com/zenOSmosis/phantom-core/issues/102 is implemented
+    const audioMediaStreamTrackCollection = this.getCollectionInstance(
+      OutputAudioMediaStreamTrackCollection
+    );
+
+    return audioMediaStreamTrackCollection.addOutputMediaStreamTrack(
+      mediaStreamTrack,
+      mediaStream
+    );
+  }
+
+  // TODO: Document
+  removeOutputMediaStreamTrack(mediaStreamTrack, mediaStream) {
+    // TODO: Use class property once https://github.com/zenOSmosis/phantom-core/issues/102 is implemented
+    const audioMediaStreamTrackCollection = this.getCollectionInstance(
+      OutputAudioMediaStreamTrackCollection
+    );
+
+    return audioMediaStreamTrackCollection.removeOutputMediaStreamTrack(
+      mediaStreamTrack,
+      mediaStream
+    );
+  }
+}
