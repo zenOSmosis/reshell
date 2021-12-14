34d25283d2523156ddb5c681891a824d4a395652
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-14 17:30:10 -0600

    Rename localStorageKey values to avoid potential conflict with non-ReShell-based Speaker.app objects

--- src/portals/SpeakerAppPortal/local/localStorageKeys.js
+++ src/portals/SpeakerAppPortal/local/localStorageKeys.js
@@ -8,21 +8,21 @@
  *
  * @type {number}
  */
-export const KEY_HISTORICAL_SESSION_COUNT = "h0";
+export const KEY_HISTORICAL_SESSION_COUNT = "h0a";
 
 /**
  * Contains local profile details.
  *
  * @type {Object}
  */
-export const KEY_LOCAL_PROFILE = "p0";
+export const KEY_LOCAL_PROFILE = "p0a";
 
 /**
  * Contains local audio defaults.
  *
  * @type {Object}
  */
-export const KEY_LOCAL_AUDIO_INPUT_DEVICES_CACHE = "ac0";
+export const KEY_LOCAL_AUDIO_INPUT_DEVICES_CACHE = "ac0a";
 
 /**
  * Contains client address and keys.  Should not be shared directly with other
@@ -30,14 +30,14 @@ export const KEY_LOCAL_AUDIO_INPUT_DEVICES_CACHE = "ac0";
  *
  * @type {Object}
  */
-export const KEY_SERVICE_AUTHORIZATION = "a0";
+export const KEY_SERVICE_AUTHORIZATION = "a0a";
 
 /**
  * Remembers cached form values from previous sessions.
  *
  * @type {Object}
  */
-export const KEY_VIRTUAL_SERVER_LOCAL_STORAGE_CREDS = "t0";
+export const KEY_VIRTUAL_SERVER_LOCAL_STORAGE_CREDS = "t0a";
 
 /**
  * Is set to true if the virtualServer manually logged out.
@@ -47,4 +47,4 @@ export const KEY_VIRTUAL_SERVER_LOCAL_STORAGE_CREDS = "t0";
  *
  * @type {boolean}
  */
-export const KEY_VIRTUAL_SERVER_DID_MANUALLY_LOGOUT = "m0";
+export const KEY_VIRTUAL_SERVER_DID_MANUALLY_LOGOUT = "m0a";
