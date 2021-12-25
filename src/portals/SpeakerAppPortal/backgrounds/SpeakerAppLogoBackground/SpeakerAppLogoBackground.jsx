bb92e55591aefb396e31d90115a1b3698237fc51
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-25 11:55:54 -0600

    Add CSS module for Speaker.app background logo (#83)

--- src/portals/SpeakerAppPortal/backgrounds/SpeakerAppLogoBackground/SpeakerAppLogoBackground.jsx
+++ src/portals/SpeakerAppPortal/backgrounds/SpeakerAppLogoBackground/SpeakerAppLogoBackground.jsx
@@ -1,34 +1,27 @@
 import React from "react";
 import Background from "@components/Background";
 import Preload from "@components/Preload";
+import classNames from "classnames";
 
 import speakerAppLogo from "../../assets/speaker.app.logo.svg";
 
-// import classNames from "classnames";
-// import styles from "./SpeakerAppLogoBackground.module.css";
+import styles from "./SpeakerAppLogoBackground.module.css";
 
-// TODO: Gradient logo
-// @see https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip
-//
-/**
- * background-clip: text;
- * -webkit-background-clip: text;
- * color: transparent;
- *     background: -webkit-linear-gradient(
--70deg
-,#db469f,#2188ff);
- */
-
-export default function SpeakerAppLogoBackground({ children, ...rest }) {
+export default function SpeakerAppLogoBackground({
+  children,
+  className,
+  ...rest
+}) {
   return (
     <Preload preloadResources={[speakerAppLogo]}>
       <Background
         {...rest}
         src={() => (
-          <div style={{ padding: 20 }}>
+          <div className={classNames(styles["logo"], className)}>
             <img src={speakerAppLogo} alt="Speaker.app" />
-            <div style={{ opacity: ".8", fontWeight: "bold" }}>
-              <span style={{ color: "orange" }}>speaker</span>.app
+            <div className={styles["text"]}>
+              <span className={styles["speaker"]}>speaker</span>
+              <span className={styles["app"]}>.app</span>
             </div>
           </div>
         )}
