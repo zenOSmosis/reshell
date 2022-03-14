d25edfea4c436d59a164b84a44a3250f4f02013c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-03-14 12:54:23 +0000

    Add TODO for Deepgram implementation

--- src/portals/HackathonPortal/apps/DeepgramSpeechRecognitionDebuggerApp/TODO.js
+++ src/portals/HackathonPortal/apps/DeepgramSpeechRecognitionDebuggerApp/TODO.js
@@ -1,1 +1,3 @@
-See: https://dev.to/devteam/join-us-for-a-new-kind-of-hackathon-on-dev-brought-to-you-by-deepgram-2bjd
+Hackathon details: https://dev.to/devteam/join-us-for-a-new-kind-of-hackathon-on-dev-brought-to-you-by-deepgram-2bjd
+
+Deepgram MediaStream implementation: https://github.com/deepgram/deepgram-node-sdk#transcribe-audio-in-real-time
