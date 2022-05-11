f1e7939686d81bd50c44aef188850ef98fd9921f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-05-11 03:24:07 +0000

    Remove prototype component

--- src/components/TransitionView/TransitionView.jsx
+++
@@ -1,7 +0,0 @@
-import Animation from "../Animation";
-
-// TODO: Document
-// TODO: Add prop-types
-// TODO: Build for arbitrarily controlled animations
-// TODO: Use forwarded ref to control Animation externally
-export default function TransitionView({}) {}
--- src/components/TransitionView/index.jsx
+++
@@ -1,3 +0,0 @@
-import TransitionView from "./TransitionView";
-
-export default TransitionView;
