85583d845096feb4a8d7f6070ce3ed79006d8391
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-05-15 18:54:07 -0500

    Prototype IP app and Network Status Monitor Service

--- src/portals/ExamplePortal/apps/IPApp/IPApp.jsx
+++ src/portals/ExamplePortal/apps/IPApp/IPApp.jsx
@@ -1,9 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
-import axios from "axios";
-
 import Layout, { Content, Footer } from "@components/Layout";
 import Center from "@components/Center";
 import Padding from "@components/Padding";
+
+import LabeledLED from "@components/labeled/LabeledLED";
+
+import NetworkStatusMonitorService from "@services/NetworkStatusMonitorService";
 
 export const REGISTRATION_ID = "ip";
 
@@ -11,42 +12,41 @@ const HelloWorldApp = {
   id: REGISTRATION_ID,
   title: "IP",
   style: {
-    width: 640,
-    height: 480,
+    width: 320,
+    height: 240,
   },
-  view: function View() {
-    const [isFetching, setIsFetching] = useState(false);
+  serviceClasses: [NetworkStatusMonitorService],
+  view: function View({ appServices }) {
+    const networkStatusMonitorService =
+      appServices[NetworkStatusMonitorService];
 
-    const handleFetchIPInfo = useCallback(() => {
-      setIsFetching(true);
-
-      // FIXME: Cancel if unmounting
-      axios
-        .get("https://www.cloudflare.com/cdn-cgi/trace")
-        .then(resp => resp.data)
-        // TODO: Parse this and display in UI
-        .then(data => console.log(data.split("\n")))
-        .finally(() => setIsFetching(false));
-    }, []);
-
-    // Automatically fetch when mounted
-    useEffect(() => {
-      handleFetchIPInfo();
-    }, [handleFetchIPInfo]);
+    const isOnline = networkStatusMonitorService.getIsOnline();
+    const isFetchingIPInfo = networkStatusMonitorService.getIsFetchingIPInfo();
+    const ipAddress = networkStatusMonitorService.getIPAddress();
 
     return (
       <Layout>
         <Content>
           <Center>
-            [TODO: Integrate call to https://www.cloudflare.com/cdn-cgi/trace]
+            <div>IP Address: {ipAddress || "N/A"}</div>
           </Center>
         </Content>
 
         <Footer>
           <Padding>
-            <button onClick={handleFetchIPInfo} disabled={isFetching}>
+            <button
+              onClick={networkStatusMonitorService.fetchIPInfo}
+              disabled={!isOnline || isFetchingIPInfo}
+              style={{ position: "absolute", left: 0, bottom: 0 }}
+            >
               Refetch
             </button>
+
+            <LabeledLED
+              style={{ float: "right" }}
+              color={isOnline ? "green" : "red"}
+              label="Internet"
+            />
           </Padding>
         </Footer>
       </Layout>
