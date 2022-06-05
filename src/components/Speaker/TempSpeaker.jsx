e1c4d8f538ea923b10953e4f4a8ae420fc03ac8a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-05 05:01:41 +0000

    Big, isolated subwoofer prototype

--- src/components/Speaker/TempSpeaker.jsx
+++ src/components/Speaker/TempSpeaker.jsx
@@ -1,8 +1,12 @@
 import "./Speaker.css";
+import AutoScaler from "../AutoScaler";
 
 export default function TempSpeaker() {
   return (
-    <div className="speaker">
+    <AutoScaler>
+      <div className="speaker">
+        {/*
+              }
       <ul className="tweeters">
         <li className="tweeter">
           <div className="cone tweeter__cone">
@@ -14,20 +18,10 @@ export default function TempSpeaker() {
           <div className="dust-cap tweeter__dust-cap"></div>
           <div className="frame tweeter__frame"></div>
         </li>
-
-        <li className="tweeter">
-          <div className="cone tweeter__cone">
-            <div className="tweeter__cone-outer"></div>
-            <div className="tweeter__cone-mid"></div>
-            <div className="tweeter__cone-inner"></div>
-            <div className="tweeter__cone-inner-shadow"></div>
-          </div>
-          <div className="dust-cap tweeter__dust-cap"></div>
-          <div className="frame tweeter__frame"></div>
-        </li>
       </ul>
+        */}
 
-      <ul className="woofers">
+        {/* <ul className="woofers"> */}
         <li className="woofer">
           <div className="cone woofer__cone">
             <div className="woofer__cone-outer"></div>
@@ -47,86 +41,8 @@ export default function TempSpeaker() {
             <li className="screw woofer__screw"></li>
           </ul>
         </li>
-
-        <li className="woofer">
-          <div className="cone woofer__cone">
-            <div className="woofer__cone-outer"></div>
-            <div className="woofer__cone-mid"></div>
-            <div className="woofer__cone-inner"></div>
-          </div>
-          <div className="dust-cap woofer__dust-cap"></div>
-          <div className="frame woofer__frame"></div>
-          <ul className="screws">
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-          </ul>
-        </li>
-
-        <li className="woofer">
-          <div className="cone woofer__cone">
-            <div className="woofer__cone-outer"></div>
-            <div className="woofer__cone-mid"></div>
-            <div className="woofer__cone-inner"></div>
-          </div>
-          <div className="dust-cap woofer__dust-cap"></div>
-          <div className="frame woofer__frame"></div>
-          <ul className="screws">
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-          </ul>
-        </li>
-
-        <li className="woofer">
-          <div className="cone woofer__cone">
-            <div className="woofer__cone-outer"></div>
-            <div className="woofer__cone-mid"></div>
-            <div className="woofer__cone-inner"></div>
-          </div>
-          <div className="dust-cap woofer__dust-cap"></div>
-          <div className="frame woofer__frame"></div>
-          <ul className="screws">
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-          </ul>
-        </li>
-
-        <li className="woofer">
-          <div className="cone woofer__cone">
-            <div className="woofer__cone-outer"></div>
-            <div className="woofer__cone-mid"></div>
-            <div className="woofer__cone-inner"></div>
-          </div>
-          <div className="dust-cap woofer__dust-cap"></div>
-          <ul className="screws">
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-            <li className="screw woofer__screw"></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+        {/* </ul> */}
+      </div>
+    </AutoScaler>
   );
 }
