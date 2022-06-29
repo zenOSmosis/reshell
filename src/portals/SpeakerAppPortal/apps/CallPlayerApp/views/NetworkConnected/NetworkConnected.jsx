b161f5a8740152e283ce6de253e54b20dc2b41a4
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-29 00:07:14 +0000

    Implement preliminary responsive layout

--- src/portals/SpeakerAppPortal/apps/CallPlayerApp/views/NetworkConnected/NetworkConnected.jsx
+++ src/portals/SpeakerAppPortal/apps/CallPlayerApp/views/NetworkConnected/NetworkConnected.jsx
@@ -8,12 +8,21 @@ import Avatar from "@components/Avatar";
 
 import ParticipantList from "./ParticipantList";
 
+import useWindowSize from "@hooks/useWindowSize";
+
+const WIDE_LAYOUT_THRESHOLD_WIDTH = 512;
+
 // TODO: Document and add prop-types
 export default function NetworkConnected({
   onOpenChat,
   localPhantomPeer,
   remotePhantomPeers = [],
 }) {
+  const windowSize = useWindowSize();
+
+  // TODO: Remove
+  console.log({ windowSize });
+
   const [selectedPhantomPeer, setSelectedPhantomPeer] =
     useState(localPhantomPeer);
 
@@ -29,9 +38,21 @@ export default function NetworkConnected({
     }
   }, [localPhantomPeer, remotePhantomPeers, selectedPhantomPeer]);
 
+  useEffect(() => {
+    if (windowSize.width < WIDE_LAYOUT_THRESHOLD_WIDTH && selectedPhantomPeer) {
+      setSelectedPhantomPeer(null);
+    }
+  }, [windowSize, selectedPhantomPeer]);
+
   return (
     <Row>
-      <Column disableHorizontalFill style={{ width: 280 }}>
+      <Column
+        disableHorizontalFill
+        style={{
+          backgroundColor: "rgba(255,255,255,.1)",
+          width: windowSize.width >= WIDE_LAYOUT_THRESHOLD_WIDTH ? 280 : "100%",
+        }}
+      >
         <ParticipantList
           localPhantomPeer={localPhantomPeer}
           remotePhantomPeers={remotePhantomPeers}
@@ -39,50 +60,56 @@ export default function NetworkConnected({
           selectedPhantomPeer={selectedPhantomPeer}
         />
       </Column>
-      {
-        // TODO: Only show this column if wide enough to show
-      }
-      <Column style={{ backgroundColor: "rgba(0,0,0,.3)" }}>
-        <Cover>
-          <Avatar
-            src={selectedPhantomPeer?.getAvatarURL()}
-            style={{
-              position: "absolute",
-              right: 10,
-              bottom: 10,
-              opacity: 0.5,
-            }}
-            size={100}
-          />
-        </Cover>
-        <Cover>
-          <Padding>
-            <Layout>
-              <Header>
-                <Row disableVerticalFill>
-                  <Column>
-                    <h1>
-                      <Ellipses>
-                        {selectedPhantomPeer?.getProfileName()}
-                      </Ellipses>
-                    </h1>
-                  </Column>
-                  <Column disableHorizontalFill>
-                    <button onClick={onOpenChat}>Chat</button>
-                  </Column>
-                </Row>
-              </Header>
-              <Content>
-                <Center canOverflow>
-                  <div style={{ fontSize: "1.5rem" }}>
-                    {selectedPhantomPeer?.getProfileDescription()}
-                  </div>
-                </Center>
-              </Content>
-            </Layout>
-          </Padding>
-        </Cover>
-      </Column>
+
+      {windowSize.width >= WIDE_LAYOUT_THRESHOLD_WIDTH && (
+        <Column style={{ backgroundColor: "rgba(0,0,0,.3)" }}>
+          <Cover>
+            <Avatar
+              src={selectedPhantomPeer?.getAvatarURL()}
+              style={{
+                position: "absolute",
+                right: 10,
+                bottom: 10,
+                opacity: 0.5,
+              }}
+              size={100}
+            />
+          </Cover>
+          <Cover>
+            {selectedPhantomPeer ? (
+              <Padding>
+                <Layout>
+                  <Header>
+                    <Row disableVerticalFill>
+                      <Column>
+                        <h1>
+                          <Ellipses>
+                            {selectedPhantomPeer.getProfileName()}
+                          </Ellipses>
+                        </h1>
+                      </Column>
+                      <Column disableHorizontalFill>
+                        <button onClick={onOpenChat}>Chat</button>
+                      </Column>
+                    </Row>
+                  </Header>
+                  <Content>
+                    <Center canOverflow>
+                      <div style={{ fontSize: "1.5rem" }}>
+                        {selectedPhantomPeer.getProfileDescription()}
+                      </div>
+                    </Center>
+                  </Content>
+                </Layout>
+              </Padding>
+            ) : (
+              <Center style={{ fontWeight: "bold" }}>
+                Select a participant to view their profile
+              </Center>
+            )}
+          </Cover>
+        </Column>
+      )}
     </Row>
   );
 }
