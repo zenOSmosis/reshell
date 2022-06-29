02a94955c9042a59ee5abb881e3574bb4c15a7d5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-29 00:25:24 +0000

    Add description ellipses to particpant tab

--- src/portals/SpeakerAppPortal/apps/CallPlayerApp/views/NetworkConnected/ParticipantList/Participant.jsx
+++ src/portals/SpeakerAppPortal/apps/CallPlayerApp/views/NetworkConnected/ParticipantList/Participant.jsx
@@ -8,6 +8,7 @@ import { Row, Column } from "@components/Layout";
 import Avatar from "@components/Avatar";
 import Padding from "@components/Padding";
 import AudioLevelMeter from "@components/audioMeters/AudioLevelMeter";
+import Ellipses from "@components/Ellipses";
 
 import MicrophoneIcon from "@icons/MicrophoneIcon";
 
@@ -27,6 +28,7 @@ export default function Participant({
   const deviceAddress = phantomPeer.getDeviceAddress();
   const avatarURL = phantomPeer.getAvatarURL();
   const profileName = phantomPeer.getProfileName();
+  const profileDescription = phantomPeer.getProfileDescription();
   const isAudioMuted = phantomPeer.getIsAudioMuted();
 
   // FIXME: "Outgoing" is in relation to the peer and is entirely confusing
@@ -47,7 +49,7 @@ export default function Participant({
   }
 
   return (
-    <ContentButton onClick={handleClick}>
+    <ContentButton onClick={handleClick} title={profileDescription}>
       <Padding>
         <Row disableVerticalFill>
           <Column disableHorizontalFill>
@@ -57,14 +59,17 @@ export default function Participant({
             <Padding style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div style={{ fontWeight: "bold" }}>{profileName}</div>
               <div>
-                {isLocalPeer && (
-                  <span className="note" style={{ fontSize: ".8rem" }}>
-                    (You)
-                  </span>
-                )}
+                <Ellipses className="note" style={{ fontSize: ".8rem" }}>
+                  {isLocalPeer && (
+                    <>
+                      <span>(You)</span>&nbsp;
+                    </>
+                  )}
+                  {profileDescription}
+                </Ellipses>
               </div>
               <div style={{ position: "absolute", right: 0, bottom: 0 }}>
-                <span style={{ marginRight: ".5rem", fontSize: ".8rem" }}>
+                <span style={{ marginRight: ".5rem", fontSize: ".6rem" }}>
                   {isAudioMuted ? "Muted" : "Unmuted"}
                 </span>
                 <MicrophoneIcon
